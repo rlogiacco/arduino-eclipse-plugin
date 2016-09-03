245d633647c8bf1ab243d8c13cef867f2889ce91
Roberto Lo Giacco <rlogiacco@gmail.com>
2016-09-03 16:28:23 +0200

    minor names refactor

--- it.baeyens.arduino.common/src/it/baeyens/arduino/common/ISerialUser.java
+++ it.baeyens.arduino.common/src/it/baeyens/arduino/common/ISerialUser.java
@@ -1,7 +1,7 @@
 package it.baeyens.arduino.common;
 
 public interface ISerialUser {
-	public boolean PauzePort(String PortName);
+	public boolean pausePort(String PortName);
 
-	public void ResumePort(String PortName);
+	public void resumePort(String PortName);
 }
